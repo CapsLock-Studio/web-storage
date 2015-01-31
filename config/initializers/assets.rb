d197b6219f1e2bf1489f5f89905d0750ea299fd4
Calvin <ts01364362@gmail.com>
2015-01-31 21:55:20 +0900

    initial commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+# Add bower_components to assets
+Rails.application.config.assets.path << Rails.root.join('app', 'assets', 'bower_components')
